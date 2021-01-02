092dff80e0caeb8db232825226d3f47ad6fac359
Kartik Mathur <kartik.mathur10@gmail.com>
2021-01-02 21:39:50 +0530

    Deep&Shallow-Copy + Copy Constructor

--- Lecture-20/OOPs.cpp
+++ Lecture-20/OOPs.cpp
@@ -3,10 +3,12 @@
 using namespace std;
 ///////////////////////// BLUEPRINT ////////////////////////////
 class Car{
+private:
+	int price;// Private members can be accessed only inside the class
 public:
 	char *name;
 	int model;
-	int price;
+	// int price;
 	int seats;
 	
 	// Constructor - We use constructor to create an Object, it helps to create an Object
@@ -26,6 +28,17 @@ public:
 		seats = s;
 	}
 
+	// Copy Constructor
+	Car(Car &X){
+		// cout<<"Inside Copy Constructor"<<endl;
+		name = new char[strlen(X.name)+1];
+		strcpy(name,X.name);
+		// name = X.name;
+		price = X.price;
+		model = X.model;
+		seats = X.seats;
+	}
+
 	// Print all the details
 	void Print(){
 		cout<<"Name : "<<name<<endl;
@@ -43,6 +56,21 @@ public:
 		name = new char[strlen(n)+1];
 		strcpy(name,n);
 	}
+	
+	// Setter
+	void setPrice(int p){
+		if(p>200){
+			price = p;
+		}
+		else{
+			price = 200;
+		}
+	}
+	
+	// Getter
+	int getPrice(){
+		return price;
+	}
 
 };
 ///////////////////////// !BLUEPRINT ////////////////////////////
@@ -53,19 +81,30 @@ int main(){
 	// A.name = "BMW";
 	// strcpy(A.name,"BMW");
 	A.UpdateName("BMW");
-	A.price = 100;
+	// A.price = 100;
+	A.setPrice(150);
 	A.model = 2020;
 	A.seats = 4;
 	// A.UpdateName("Maruti");
 	A.Print();
 	// cout<<A.name<<endl;
 	// cout<<A.price<<endl;
+	cout<<A.getPrice()<<endl;
 	// cout<<A.model<<endl;
 	// cout<<A.seats<<endl;
 	// char x[100]="Audi";
 	Car B("Audi",2019,150,2);
 	
+	
+	Car C = B; // Car C(B);
+	Car D = B; // Car C(B);
+
+	C.name[0] = 'B';
+
+
 	B.Print();
+	C.Print();
+	D.Print();
 	
 	return 0;
 }
