552e84c273f9ca5025524f90f306e46447f6eb58
Kartik Mathur <kartik.mathur10@gmail.com>
2021-01-02 20:53:13 +0530

    UpdateName Function OOPs

--- Lecture-20/OOPs.cpp
+++ Lecture-20/OOPs.cpp
@@ -4,7 +4,7 @@ using namespace std;
 ///////////////////////// BLUEPRINT ////////////////////////////
 class Car{
 public:
-	char name[100];
+	char *name;
 	int model;
 	int price;
 	int seats;
@@ -12,12 +12,14 @@ public:
 	// Constructor - We use constructor to create an Object, it helps to create an Object
 	// 1. Default Constructor
 	Car(){
+		name = NULL;
 		// cout<<"In Default Constructor"<<endl;
 	}
 
 	// 2. Parameterized Constructor
 	Car(char *n,int m,int p,int s){
 		// cout<<"Inside Parameterized Constructor"<<endl;
+		name = new char[strlen(n)+1];
 		strcpy(name,n);
 		model = m;
 		price = p;
@@ -32,6 +34,16 @@ public:
 		cout<<"Seats: "<<seats<<endl<<endl;
 	}
 
+	void UpdateName(char *n){
+		if(name != NULL){
+			// There is some name already
+			delete []name;
+			name = NULL;
+		}
+		name = new char[strlen(n)+1];
+		strcpy(name,n);
+	}
+
 };
 ///////////////////////// !BLUEPRINT ////////////////////////////
 
@@ -39,10 +51,12 @@ int main(){
 	Car A; // This takes space, when object is created constructor is called
 	// Constructor creates the object, and it gets called when the object is created
 	// A.name = "BMW";
-	strcpy(A.name,"BMW");
+	// strcpy(A.name,"BMW");
+	A.UpdateName("BMW");
 	A.price = 100;
 	A.model = 2020;
 	A.seats = 4;
+	// A.UpdateName("Maruti");
 	A.Print();
 	// cout<<A.name<<endl;
 	// cout<<A.price<<endl;
