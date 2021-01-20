93e34be0f321df4cf235c945720fb10024de345a
Kartik Mathur <kartik.mathur10@gmail.com>
2021-01-20 20:46:59 +0530

    Lecture-28

--- Lecture-28/BinaryTrees.cpp
+++ Lecture-28/BinaryTrees.cpp
@@ -31,7 +31,7 @@ void preOrder(node* root) {
 	}
 
 	// recursive case
-	cout << root->data << " ";
+	cout << root->data << ",";
 	preOrder(root->left);
 	preOrder(root->right);
 }
@@ -46,7 +46,7 @@ void inOrder(node* root) {
 	// 1. Print left sub tree first
 	inOrder(root->left);
 	// 2. Print the root node
-	cout << root->data << " ";
+	cout << root->data << ",";
 	// 3. Print the right sub tree
 	inOrder(root->right);
 }
@@ -118,6 +118,71 @@ void PrintLevel(node* root) {
 
 /////////////////////////////// LEVEL ORDER //////////////////////////
 
+////////////////////////////// Create Tree Level Wise ////////////////
+node* BuildTreeLevel() {
+	node* root = NULL;
+	int data;
+	cout << "Enter root data ";
+	cin >> data;
+	if (data == -1) {
+		return NULL;
+	}
+	root = new node(data);
+
+	queue<node*> q;
+	q.push(root);
+	while (!q.empty()) {
+		node* x = q.front();
+		q.pop();
+
+		cout << "Enter data of " << x->data << endl;
+		int left_data, right_data;
+		cin >> left_data >> right_data;
+		if (left_data != -1) {
+			x->left = new node(left_data);
+			q.push(x->left);
+		}
+		if (right_data != -1) {
+			x->right = new node(right_data);
+			q.push(x->right);
+		}
+	}
+
+	return root;
+}
+////////////////////////////// Create Tree Level Wise ////////////////
+
+//////////////////////// TREE FROM IN and PRE ///////////////////////
+int pre[] = {8, 3, 1, 6, 4, 7, 10, 14, 13};
+int i = 0;
+int in[] = {1, 3, 4, 6, 7, 8, 10, 13, 14};
+
+node* CreateTree(int s, int e) {
+	// Base case
+	if (s > e) {
+		return NULL;
+	}
+
+	// Recursive case
+	int d = pre[i++];
+	node* root = new node(d);
+	int k = -1;
+	for (int j = s ; j <= e ; j++) {
+		if (in[j] == d) {
+			k = j;
+			break;
+		}
+	}
+
+	root->left = CreateTree(s, k - 1);
+	root->right = CreateTree(k + 1, e);
+
+	return root;
+}
+
+//////////////////////// TREE FROM IN and PRE ///////////////////////
+
+
 int main() {
 
 #ifndef ONLINE_JUDGE
@@ -125,15 +190,20 @@ int main() {
 	freopen("output.txt", "w", stdout);
 #endif
 
-	node* root = BuildTree();
+	// node* root = BuildTree();
+	int n = sizeof(in) / sizeof(int);
+	node* root = CreateTree(0, n - 1);
+	// node* root = BuildTreeLevel();
 	// mirror(root);
-	// preOrder(root);
-	// cout << endl;
+	preOrder(root);
+	cout << endl;
+
+	inOrder(root);
+	cout << endl;
+
 	PrintLevel(root);
-	// inOrder(root);
-	// cout << endl;
-	// postOrder(root);
-	// cout << endl;
+	postOrder(root);
+	cout << endl;
 	// cout << "Total Node Count: " << CountNodes(root) << endl;
 	// cout << "Height: " << height(root) << endl;
 	// cout << "Diameter: " << diameter(root) << endl;
