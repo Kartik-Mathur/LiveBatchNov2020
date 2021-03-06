a9f9bd5d77139421511e52143a1d36a53c9228f9
Kartik Mathur <kartikmathur@Kartiks-MacBook-Pro.local>
2021-03-06 22:09:41 +0530

    Pasha And Strings

---
+++ Lecture-43/PashaAndStrings.cpp
@@ -0,0 +1,36 @@
+// PashaAndStrings.cpp
+#include <iostream>
+using namespace std;
+
+char a[200002];
+
+void solve(int m) {
+	int len = strlen(a);
+	int count[100001] = {0};
+
+	for (int i = 0 ; i < m ; i++) {
+		int ai; cin >> ai;
+		count[ai - 1]++;
+	}
+
+	long long int sum = 0;
+	for (int i = 0 ; i < len / 2 ; i++) {
+		sum += count[i];
+		if (sum % 2 == 1) {
+			swap(a[i], a[len - 1 - i]);
+		}
+	}
+	cout << a << endl;
+}
+
+int main() {
+
+	cin >> a;
+
+	int m;
+	cin >> m;
+
+	solve(m);
+
+	return 0;
+}
